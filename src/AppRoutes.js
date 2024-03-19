edb8b7bcf28cd6bf452172301185b66534d6e385
Pawan_PC <pawan_pc@localhost.localdomain>
2024-03-19 10:18:10 +0530

    new nav

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -45,6 +45,103 @@ const AppRoutes = () => {
                 </Layout>
               }
             />
+            <Route
+              path="civil"
+              element={
+                <Layout>
+                  <Department comp="civil" />
+                </Layout>
+              }
+            />
+            <Route
+              path="EIC"
+              element={
+                <Layout>
+                  <Department comp="EIC" />
+                </Layout>
+              }
+            />
+            <Route
+              path="ECE"
+              element={
+                <Layout>
+                  <Department comp="ECE" />
+                </Layout>
+              }
+            />
+            <Route
+              path="mechanical"
+              element={
+                <Layout>
+                  <Department comp="mechanical" />
+                </Layout>
+              }
+            />
+            <Route
+              path="electrical"
+              element={
+                <Layout>
+                  <Department comp="electrical" />
+                </Layout>
+              }
+            />
+            <Route
+              path="mca"
+              element={
+                <Layout>
+                  <Department comp="mca" />
+                </Layout>
+              }
+            />
+            <Route
+              path="mba"
+              element={
+                <Layout>
+                  <Department comp="mba" />
+                </Layout>
+              }
+            />
+            <Route
+              path="physics"
+              element={
+                <Layout>
+                  <Department comp="physics" />
+                </Layout>
+              }
+            />
+            <Route
+              path="chemistry"
+              element={
+                <Layout>
+                  <Department comp="chemistry" />
+                </Layout>
+              }
+            />
+            <Route
+              path="maths"
+              element={
+                <Layout>
+                  <Department comp="maths" />
+                </Layout>
+              }
+              
+            />
+            <Route
+              path="english"
+              element={
+                <Layout>
+                  <Department comp="english" />
+                </Layout>
+              }
+            />
+            <Route
+              path="economics"
+              element={
+                <Layout>
+                  <Department comp="economics" />
+                </Layout>
+              }
+            />
             <Route path="*" element={<Error />} />
           </Route>
         </Routes>
