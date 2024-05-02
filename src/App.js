bda674d35e1ad42fbe30a4f291825712b829edf0
Prince Vashisth <vashisthprince9@gmail.com>
2024-05-02 23:34:50 +0530

    some mediaquerynew

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import AppRoutes from "./AppRoutes";
 
 function App() {
   const handelchange=()=>{
-    
+
   }
   return (
     <>
       {/* ----------- Our all pages routes section  */}
       <AppRoutes />
-      <div style={{position:'fixed',top:'50%'}}>
+      <div style={{position:'fixed',top:'50%',left:-100}}>
       <div style={{display:'flex',alignItems:'center',backgroundColor:'red',gap:10,width:'130px',justifyContent:'space-between'}} onMouseOver={handelchange} ><span>Youtube</span>  <YouTube/>  </div>
       <div style={{display:'flex',alignItems:'center',backgroundColor:'skyblue',gap:10,width:'130px',justifyContent:'space-between'}}  onMouseOver={handelchange}><span>Linkedin</span> <LinkedIn/> </div>
-      <div style={{display:'flex',alignItems:'center',backgroundColor:'blue',gap:10,width:'130px',justifyContent:'space-between'}}  onMouseOver={handelchange}><span>Facebook</span> <Facebook/> </div>
+      <div style={{display:'flex',alignItems:'center',backgroundColor:'blue',gap:10,width:'130px',justifyContent:'space-between',left:'-100px'}}  onMouseOver={handelchange}><span>Facebook</span> <Facebook/> </div>
       </div>
     </>
   );
